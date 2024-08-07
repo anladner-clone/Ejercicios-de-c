0ced71a2e7f3e90d4d1f1df8bafb60a418064ce9
Ariel Ladner <155122182+anladner-clone@users.noreply.github.com>
2024-08-07 15:24:03 -0300

    Create TP_5_Ejercicio_11.cpp

---
+++ TP_5_Ejercicio_11.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <conio.h>
+#include <time.h>
+
+int vectorDeEstadistica[10];
+
+int main(){
+	system("color b");
+	int num,indice;
+	srand(time(NULL)); // inicializar n° aleat.   
+	for(int i = 0; i < 80; i++){
+		num = 1 + rand() % (99);
+		indice = num/10;
+		vectorDeEstadistica[indice]+=1;
+	}
+
+	for(int linea = 0; linea < 10; linea++){
+		for(int caracter = 0; caracter < vectorDeEstadistica[linea]; caracter++){
+			if(caracter==0)printf("\n\tDecena %d: ", linea);
+			printf(" * ");
+		}
+		printf("\n\t");
+	}
+}
